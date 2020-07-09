e19b6eba4e7ef0cbbafdbfc36509e4537ca48057
Alice Yeh <aliceyeh12@gmail.com>
2020-07-09 06:37:07 -0700

    Draft

---
+++ calc.py
@@ -0,0 +1,7 @@
+print("DYSFUNCTIONAL CALCULATOR")
+
+first_num = int(input("Enter a number: "))
+second_num = int(input("Enter another number: "))
+operand = = input("Enter an operand: ")
+
+print("Thanks, expect a response in 3-8 business days.")
